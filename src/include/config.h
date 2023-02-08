9d29a0f543329d97fd67401b6906c63a125befe0
Ulf Samuelsson <ulf@emagii.com>
2023-02-08 17:14:07 +0100

    Updated autotools files
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

--- src/include/config.h
+++ src/include/config.h
@@ -1,119 +1,120 @@
+/* src/include/config.h.  Generated from config.h.in by configure.  */
 /* src/include/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the <compositor.h> header file. */
-#undef HAVE_COMPOSITOR_H
+/* #undef HAVE_COMPOSITOR_H */
 
 /* Define to 1 if you have the <config-parser.h> header file. */
-#undef HAVE_CONFIG_PARSER_H
+/* #undef HAVE_CONFIG_PARSER_H */
 
 /* Define to 1 if you have the <ctype.h> header file. */
-#undef HAVE_CTYPE_H
+#define HAVE_CTYPE_H 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#undef HAVE_FCNTL_H
+#define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <libevdev-1.0/libevdev/libevdev.h> header file.
    */
-#undef HAVE_LIBEVDEV_1_0_LIBEVDEV_LIBEVDEV_H
+#define HAVE_LIBEVDEV_1_0_LIBEVDEV_LIBEVDEV_H 1
 
 /* Define to 1 if you have the <linux/input.h> header file. */
-#undef HAVE_LINUX_INPUT_H
+#define HAVE_LINUX_INPUT_H 1
 
 /* Define to 1 if you have the <matrix.h> header file. */
-#undef HAVE_MATRIX_H
+/* #undef HAVE_MATRIX_H */
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <pixman.h> header file. */
-#undef HAVE_PIXMAN_H
+/* #undef HAVE_PIXMAN_H */
 
 /* Define to 1 if you have the <stdarg.h> header file. */
-#undef HAVE_STDARG_H
+#define HAVE_STDARG_H 1
 
 /* Define to 1 if you have the <stdbool.h> header file. */
-#undef HAVE_STDBOOL_H
+#define HAVE_STDBOOL_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdio.h> header file. */
-#undef HAVE_STDIO_H
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <timeline-object.h> header file. */
-#undef HAVE_TIMELINE_OBJECT_H
+/* #undef HAVE_TIMELINE_OBJECT_H */
 
 /* Define to 1 if you have the <time.h> header file. */
-#undef HAVE_TIME_H
+#define HAVE_TIME_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to 1 if you have the <wayland-server.h> header file. */
-#undef HAVE_WAYLAND_SERVER_H
+#define HAVE_WAYLAND_SERVER_H 1
 
 /* Define to 1 if you have the <weston.h> header file. */
-#undef HAVE_WESTON_H
+/* #undef HAVE_WESTON_H */
 
 /* Define to 1 if you have the <xkbcommon/xkbcommon.h> header file. */
-#undef HAVE_XKBCOMMON_XKBCOMMON_H
+#define HAVE_XKBCOMMON_XKBCOMMON_H 1
 
 /* Define to 1 if you have the <zalloc.h> header file. */
-#undef HAVE_ZALLOC_H
+/* #undef HAVE_ZALLOC_H */
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
+#define LT_OBJDIR ".libs/"
 
 /* Description */
-#undef OS_LINUX
+/* #undef OS_LINUX */
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "Ulf Samuelsson"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "libweston-binder"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "libweston-binder 1.0"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "libweston-binder-1.0"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL "http://www.emagii.com/"
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "1.0"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#undef TIME_WITH_SYS_TIME
+#define TIME_WITH_SYS_TIME 1
 
 /* Define to empty if `const' does not conform to ANSI C. */
-#undef const
+/* #undef const */
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
-#undef size_t
+/* #undef size_t */
