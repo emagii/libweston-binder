78defba495a38e461aa30de3f1627e0fd2796153
Ulf Samuelsson <ulf@emagii.com>
2023-02-11 21:27:40 +0100

    Update configuration
    Signed-off-by: Ulf Samuelsson <ulf@emagii.com>

--- src/include/config.h
+++ src/include/config.h
@@ -96,16 +96,16 @@
 #define PACKAGE_NAME "libweston-binder"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libweston-binder 1.0"
+#define PACKAGE_STRING "libweston-binder 1.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "libweston-binder-1.0"
+#define PACKAGE_TARNAME "libweston-binder-1.0.0"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL "http://www.emagii.com/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0"
+#define PACKAGE_VERSION "1.0.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
